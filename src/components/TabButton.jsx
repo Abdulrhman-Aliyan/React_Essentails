8ee15472b77873f803d71ea2e906e348b23b754a
Abdulrhman Aliyan <aboood1999@live.com>
2024-03-30 19:56:52 +0300

    using ...prop in taqbButton and section to ward any property used on them to use them as section component

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,4 +1,4 @@
-export default function TabButton({children, onSelect, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
 
     function handleClick() {
         console.log("Hello World - Clicked!");
@@ -6,7 +6,7 @@ export default function TabButton({children, onSelect, isSelected}) {
 
     return (
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
         </li>
     )
 }
